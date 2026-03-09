48d34293e53ef5744d23f7105b943b86dee2efc1
Zaterval <ziang_zheng@foxmail.com>
2026-03-09 14:33:07 +0800

    clear robot assets config

--- source/genesis_assets/genesis_assets/robots/unitree/__init__.py
+++ source/genesis_assets/genesis_assets/robots/unitree/__init__.py
@@ -9,15 +9,11 @@ This package provides configurations for various Unitree robots including:
 """
 
 from .b2 import UNITREE_B2_CFG
-from .g1_23dof import UNITREE_G1_23DOF_CFG
 from .go2 import UNITREE_GO2_CFG
-from .go2w import UNITREE_GO2W_CFG
 from .h1 import UNITREE_H1_CFG
 
 __all__ = [
     "UNITREE_GO2_CFG",
-    "UNITREE_GO2W_CFG",
     "UNITREE_B2_CFG",
     "UNITREE_H1_CFG",
-    "UNITREE_G1_23DOF_CFG",
 ]
